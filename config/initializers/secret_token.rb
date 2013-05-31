4a0e7c5d19cd343015e73ac615d5d51ca99821ee
Eric Cumbee <ecumbee@gmail.com>
2013-05-31 17:41:29 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phonebankapplication::Application.config.secret_token = '8debb7de4a091dff9e2e5b6dc941a3de3901f518920f456c04f3287b556dd1b62eb7dc94407a9da3c77cb4c74d03c9617a925211f35f8e223880ef99ea6a5a0b'
